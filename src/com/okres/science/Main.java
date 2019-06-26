0f36c0ace7fc5808a565e52404344018c2afe935
sifuua <alekseykres@gmail.com>
2019-06-27 01:17:32 +0300

    Initial commit

---
+++ src/com/okres/science/Main.java
@@ -0,0 +1,63 @@
+package com.okres.science;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(karatsuba(5437, 9845));
+    }
+
+    private static int karatsuba(int x, int y) {
+
+        int n = Math.max(
+                Integer.valueOf(x).toString().length(),
+                Integer.valueOf(y).toString().length());
+        if (n == 1) {
+            return x * y;
+        } else {
+            int m = n / 2 + n % 2;
+
+            int a = x / (int) Math.pow(10, m);
+            int b = x % (int) Math.pow(10, m);
+            int c = y / (int) (Math.pow(10, m));
+            int d = y % (int) (Math.pow(10, m));
+
+            int p = a + b;
+            int q = c + d;
+
+            /*recursive call*/
+            int ac = karatsuba(a, c);
+            int bd = karatsuba(b, d);
+            int pq = karatsuba(p, q);
+
+            int abcd = pq - ac - bd;
+            return (int) (Math.pow(10, n) * ac + Math.pow(10, n / 2) * abcd + bd);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      /*  if (x < 10 && y < 10) {
+            return x * y;
+        }
+
+        int n = Math.max(
+                Integer.valueOf(x).toString().length(),
+                Integer.valueOf(y).toString().length());
+
+
+
+        return 0;*/
+    }
+}
