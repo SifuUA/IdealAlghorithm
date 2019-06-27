37ac6468d0889f5f99cd0d03f42d418b2efa628c
Oleksii Kres <oleksii_kres@epam.com>
2019-06-27 15:43:09 +0300

    some optimization

--- src/com/okres/karatsuba/Main.java
+++ src/com/okres/karatsuba/Main.java
@@ -3,61 +3,32 @@ package com.okres.karatsuba;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(karatsuba(5437, 9845));
+        long currentTimeMillisBefore = System.currentTimeMillis();
+        System.out.println(karatsuba(543712312912999999L, 9849929259292929L));
+        long currentTimeMillisAfter = System.currentTimeMillis();
+        System.out.println(currentTimeMillisAfter - currentTimeMillisBefore);
     }
 
-    private static int karatsuba(int x, int y) {
-
-        int n = Math.max(
-                Integer.toString(x).length(),
-                Integer.toString(y).length());
+    private static long karatsuba(long x, long y) {
+        long n = Math.max(
+                Long.toString(x).length(),
+                Long.toString(y).length());
         if (n == 1) {
             return x * y;
         } else {
-            int m = n / 2 + n % 2;
+            long m = n / 2 + n % 2;
 
-            int a = x / (int) Math.pow(10, m);
-            int b = x % (int) Math.pow(10, m);
-            int c = y / (int) (Math.pow(10, m));
-            int d = y % (int) (Math.pow(10, m));
+            long a = x / (long) Math.pow(10, m);
+            long b = x % (long) Math.pow(10, m);
+            long c = y / (long) (Math.pow(10, m));
+            long d = y % (long) (Math.pow(10, m));
 
-            int p = a + b;
-            int q = c + d;
+            /*recursive calls*/
+            long ac = karatsuba(a, c);
+            long bd = karatsuba(b, d);
+            long pq = karatsuba((a + b), (c + d));
 
-            /*recursive call*/
-            int ac = karatsuba(a, c);
-            int bd = karatsuba(b, d);
-            int pq = karatsuba(p, q);
-
-            int abcd = pq - ac - bd;
-            return (int) ((Math.pow(10, 2 * m) * ac) + (Math.pow(10, m) * abcd) + bd);
+            return (long) ((Math.pow(10, 2 * m) * ac) + (Math.pow(10, m) * (pq - ac - bd)) + bd);
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      /*  if (x < 10 && y < 10) {
-            return x * y;
-        }
-
-        int n = Math.max(
-                Integer.valueOf(x).toString().length(),
-                Integer.valueOf(y).toString().length());
-
-
-
-        return 0;*/
     }
 }
